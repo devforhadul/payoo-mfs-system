48115b9eae2d23cb22ebad1c38826fe9666a82cd
Forhadul Islam <forhadul75@gmail.com>
2025-02-25 11:33:49 +0300

    Ad more work

--- Scripts/addmoney.js
+++ Scripts/addmoney.js
@@ -5,14 +5,22 @@ document.getElementById('addBtn').addEventListener('click', function (event) {
     const addAmount = parseInt(document.getElementById('addAmount').value);
     const addPin = parseInt(document.getElementById('addPin').value);
     const mainBalance = parseInt(document.getElementById('mainBalance').innerText);
-    ;
-    if( addPin === 1122){
+
+
+    if (addPin === 1122) {
         let totalBalance = mainBalance + addAmount;
         document.getElementById("mainBalance").innerText = totalBalance;
+        // trx
+        document.getElementById("trxName").innerText = 'Add Money';
+        document.getElementById("trxAddB").innerText = 'Added: ' + addAmount;
+        document.getElementById("trxNewB").innerText = totalBalance;
+        document.getElementById("trxDate").innerText = new Date().toLocaleString();
     }
-    else{
+    else {
         alert("Enter right PIN Number")
     }
+
+
 
 
 })
--- Scripts/toggle.js
+++ Scripts/toggle.js
@@ -3,15 +3,27 @@ document.getElementById("addMoneyBtn").addEventListener('click', function(){
 
     document.getElementById("addMoney").style.display = 'block';
     document.getElementById("cashOut").style.display = 'none';
-    
+    document.getElementById("trxBox").style.display = 'none';
 
 })
 
 document.getElementById("cashOutBtn").addEventListener('click', function(){
     document.getElementById("cashOut").style.display = 'block';
     document.getElementById("addMoney").style.display = 'none';
+    document.getElementById("trxBox").style.display = 'none';
+    
 })
+
+document.getElementById("trxBtn").addEventListener('click', function(){
+    document.getElementById("cashOut").style.display = 'none';
+    document.getElementById("addMoney").style.display = 'none';
+    document.getElementById("trxBox").style.display = 'block';
+})
+
+
+
 
 document.getElementById("logOutBtn").addEventListener('click', function(){
     window.location.href = './index.html'
 })
+
